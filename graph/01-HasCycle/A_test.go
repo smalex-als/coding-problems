b7ac2d86b76b5b42559f8611afcce95b4455ea48
smalex-als <smalex69@gmail.com>
2020-10-24 10:06:15 -0400

    added graph tests

--- graph/01-HasCycle/A_test.go
+++ graph/01-HasCycle/A_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"coding-problems/test"
+	"math/rand"
 	"testing"
 )
 
@@ -10,16 +11,21 @@ func TestA(t *testing.T) {
 }
 
 func TestBIG(t *testing.T) {
-	g := GenGraph(15)
+	g := GenGraphTree(20)
 	t.Log("Big", len(g))
 	if hasCycle(len(g), g) {
 		t.Fail()
 	}
+	u := rand.Intn(len(g))
+	v := rand.Intn(len(g))
+	g[u] = append(g[u], v)
+	if !hasCycle(len(g), g) {
+		t.Fail()
+	}
 }
 
-var nextId int
-
-func GenGraph(level int) [][]int {
+func GenGraphTree(level int) [][]int {
+	nextId := 0
 	n := 0
 	cur := 1
 	for i := 0; i < level; i++ {
@@ -30,17 +36,17 @@ func GenGraph(level int) [][]int {
 	for i := 0; i < n; i++ {
 		g[i] = make([]int, 0)
 	}
-	GenGraphDfs(g, 0, level)
+	GenGraphTreeDfs(g, 0, level, &nextId)
 	return g
 }
 
-func GenGraphDfs(g [][]int, from int, level int) {
+func GenGraphTreeDfs(g [][]int, from int, level int, nextId *int) {
 	if level == 1 {
 		return
 	}
 	for i := 0; i < 2; i++ {
-		nextId++
-		g[from] = append(g[from], nextId)
-		GenGraphDfs(g, nextId, level-1)
+		*nextId++
+		g[from] = append(g[from], *nextId)
+		GenGraphTreeDfs(g, *nextId, level-1, nextId)
 	}
 }
