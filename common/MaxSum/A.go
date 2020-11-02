b72ba914c45c239886817c1c7a20c929eb0ea2ce
Smychagin Alexey <smalex69@gmail.com>
2020-11-02 09:32:48 -0500

    add MaxSum problem

---
+++ common/MaxSum/A.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve() {
+	n := readInt()
+	a := readIntArray(n)
+	cur := 0
+	ans := 0
+	for _, v := range a {
+		cur += v
+		if v > cur {
+			cur = v
+		}
+		if cur > ans {
+			ans = cur
+		}
+	}
+	fmt.Println(ans)
+}
+
+var scanner *bufio.Scanner
+
+func main() {
+	scanner = bufio.NewScanner(os.Stdin)
+	const MaxTokenLength = 1024 * 1024
+	scanner.Buffer(make([]byte, 0, MaxTokenLength), MaxTokenLength)
+	scanner.Split(bufio.ScanWords)
+	solve()
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func min(a int64, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// IO
+
+func readString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func readInt() int {
+	val, _ := strconv.Atoi(readString())
+	return val
+}
+
+func readInt64() int64 {
+	v, _ := strconv.ParseInt(readString(), 10, 64)
+	return v
+}
+
+func readIntArray(sz int) []int {
+	a := make([]int, sz)
+	for i := 0; i < sz; i++ {
+		a[i] = readInt()
+	}
+	return a
+}
+
+func readInt64Array(n int) []int64 {
+	res := make([]int64, n)
+	for i := 0; i < n; i++ {
+		res[i] = readInt64()
+	}
+	return res
+}
